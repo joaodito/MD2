8928134b7292f5e32eb8cea6a62bf5bf00a314ae
João Costa <58365406+joaodito@users.noreply.github.com>
2022-09-08 16:25:52 -0300

    Update chineseTheory.cpp

--- chineseTheory.cpp
+++ chineseTheory.cpp
@@ -4,10 +4,10 @@ using namespace std;
 //A solucao consiste em achar solucao do numero que possa ser dividido por x numeros com restos i
 //Nesse caso é necessário indicar os valores dos divisores dentro do vetor 'dividePor' e informar os restos dentro do vetor'restoEsperado'
 
-int TCR(int dividePor[], int restoEsperado[], int i){
+int tcr(int dividePor[], int restoEsperado[], int i){
 	int valor = 1;
     do{
-			int n;
+	int n;
             for( n = 0 ; n < i; n++){
                 if(valor % dividePor[n] != restoEsperado [n]){ 
                      break;
@@ -25,8 +25,7 @@ int main(void){
 	int restoEsperado[]={2,3,4};
         
     int i = sizeof(dividePor) / sizeof (dividePor[0]);
-    
-    printf("A solucao possível é:"); 
-    TCR(dividePor, restoEsperado, i);
-        return 0;
+    tcr(dividePor, restoEsperado, i); 
+    printf("A solucao possível é: %d", tcr); 
+    return 0;
 }
